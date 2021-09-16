a6ad08c7a86cb22c52dd5a637ef7188e63ed67d1
Fira <firafitriatun@gmail.com>
2021-09-16 11:09:32 +0800

    cctv under recontruction

---
+++ app/Response.kt
@@ -0,0 +1,27 @@
+data class Response(
+
+	@field:SerializedName("data")
+	val data: List<DataItem>,
+
+	@field:SerializedName("message")
+	val message: String
+)
+
+data class DataItem(
+
+	@field:SerializedName("nama")
+	val nama: String,
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String,
+
+	@field:SerializedName("created_at")
+	val createdAt: String,
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("url")
+	val url: String
+)
+
---
+++ app/src/main/java/com/dicoding/panbas/data/datasource/response/CctvResponse.kt
@@ -0,0 +1,30 @@
+package com.dicoding.panbas.data.datasource.response
+
+import com.google.gson.annotations.SerializedName
+
+data class CctvResponse(
+
+	@field:SerializedName("data")
+	val data: List<DataItem>,
+
+	@field:SerializedName("message")
+	val message: String
+)
+
+data class DataItem(
+
+	@field:SerializedName("nama")
+	val nama: String,
+
+	@field:SerializedName("updated_at")
+	val updatedAt: String,
+
+	@field:SerializedName("created_at")
+	val createdAt: String,
+
+	@field:SerializedName("id")
+	val id: Int,
+
+	@field:SerializedName("url")
+	val url: String
+)
