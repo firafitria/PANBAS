fab12fde218f620639b8e41c1264cec94cd0eaa3
Fira <firafitriatun@gmail.com>
2021-09-22 12:15:34 +0800

    live data implemented and change some code

--- app/src/main/java/com/dicoding/panbas/ui/detail_report/DetailReportActivity.kt
+++ app/src/main/java/com/dicoding/panbas/ui/detail_report/DetailReportActivity.kt
@@ -1,7 +1,11 @@
 package com.dicoding.panbas.ui.detail_report
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.dicoding.panbas.R
@@ -20,36 +24,36 @@ class DetailReportActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailReportBinding
     private lateinit var viewModel: DetailReportViewModel
+    private var detailReport: ReportEntity? = null
+    private lateinit var mutableDetailReport: LiveData<ReportEntity>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityDetailReportBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    override fun onStart(){
+        super.onStart()
         val factory = ViewModelFactory.getInstance(this)
-        val viewModel = ViewModelProvider(this, factory)[DetailReportViewModel::class.java]
+        viewModel = ViewModelProvider(this, factory)[DetailReportViewModel::class.java]
+        viewModel.setReport(intent.getParcelableExtra(REPORT))
 
-        val extras = intent.extras
-        if (extras != null) {
-            val report = extras.getParcelable<ReportEntity>(REPORT)
-            if (report!= null) {
-                viewModel.setReport(report.idreport)
-                populateReport(viewModel.getItemReport())
+        showDetailReport()
 
-            }
-        }
     }
 
-    private fun populateReport(report: ReportEntity) {
-        with(binding) {
-            tvName.text = report.name
-            tvWaktu.text = report.time
-            Glide.with(binding.root).load(report.imagePath)
+    private fun showDetailReport() {
+        viewModel.getReport().observe(this, {
+            binding.tvLokasi.text = it.lokasi
+            binding.tvKet.text = it.keterangan
+            Glide.with(this@DetailReportActivity)
+                .load(it.linkFoto)
                 .into(binding.ivReport)
-            tvLokasi.text = report.location
-            tvKet.text = report.info
 
-        }
+        })
 
     }
+
 }
