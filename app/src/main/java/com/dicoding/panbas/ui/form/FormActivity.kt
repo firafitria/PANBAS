edcdbb9e2f8a21e62f0972db50c57d8801c604ed
Fira <firafitriatun@gmail.com>
2021-09-22 12:17:17 +0800

    belum selesai nich

--- app/src/main/java/com/dicoding/panbas/ui/form/FormActivity.kt
+++ app/src/main/java/com/dicoding/panbas/ui/form/FormActivity.kt
@@ -8,50 +8,75 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.bumptech.glide.Glide
+import com.dicoding.panbas.BuildConfig
 import com.dicoding.panbas.R
 import com.dicoding.panbas.databinding.ActivityDetailBinding
 import com.dicoding.panbas.databinding.ActivityFormBinding
 import com.dicoding.panbas.ml.Model
 import org.tensorflow.lite.support.image.TensorImage
+import java.io.File
 
 class FormActivity : AppCompatActivity() {
     private lateinit var binding: ActivityFormBinding
     lateinit var bitmap: Bitmap
+    private val AUTHORITY = BuildConfig.APPLICATION_ID + ".provider"
+    private lateinit var imageUri : Uri //uri lokasi dari foto
+    private lateinit var output : File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityFormBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         with(binding){
             imageButton.setOnClickListener(View.OnClickListener {
                 var camera : Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                imageUri = FileProvider.getUriForFile(this@FormActivity, AUTHORITY, output)
+                imageUri.path?.let { Log.d("Photos", it) }
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
                 startActivityForResult(camera, 200) })
-
             button3.setOnClickListener(View.OnClickListener {
                     var resized = Bitmap.createScaledBitmap(bitmap, 224, 224, true)
                     val model = Model.newInstance(this@FormActivity)
-
-
                     var tbuffer = TensorImage.fromBitmap(resized)
                     var byteBuffer = tbuffer.buffer
-
 // Creates inputs for reference.
                     val image = TensorImage.fromBitmap(bitmap)
-
 // Runs model inference and gets result.
                     val outputs = model.process(image)
                     val probability = outputs.probabilityAsCategoryList
-
-            tvPrediksi.text = probability.toString()
-
+                     tvPrediksi.text = probability.toString()
 // Releases model resources if no longer used.
                     model.close()
                 })
+
+            buttonBuat.setOnClickListener {
+                kirimForm()
+            }
         }
     }
+
+    private fun kirimForm() {
+        binding.apply{
+            when{
+                textInputLokasi.helperText?.isEmpty() == true -> {
+                    textInputLokasi.error = "Kolom harus diisi"
+                    textInputLokasi.requestFocus()
+                    return
+                }
+                textInputKeterangan.helperText?.isEmpty() == true -> {
+                    textInputKeterangan.error = "Kolom harus diisi"
+                    textInputKeterangan.requestFocus()
+                    return
+                }
+            }
+        }
+    }
+
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
--- app/src/main/java/com/dicoding/panbas/ui/form/FormViewModel.kt
+++ app/src/main/java/com/dicoding/panbas/ui/form/FormViewModel.kt
@@ -1,4 +1,10 @@
 package com.dicoding.panbas.ui.form
 
-class FormViewModel {
+import androidx.lifecycle.ViewModel
+import com.dicoding.panbas.data.datasource.remote.request.ReportRequest
+import com.dicoding.panbas.data.repository.report.ReportRepository
+
+class FormViewModel(private val report: ReportRepository): ViewModel (){
+   // fun createReport(request: ReportRequest) = report.createReport(request)
+
 }
