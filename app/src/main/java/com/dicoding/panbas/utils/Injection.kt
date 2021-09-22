e53498a45e569cbecab421ed6c843780773a0b9e
Fira <firafitriatun@gmail.com>
2021-09-22 12:18:12 +0800

    add database, dao, and app executor

--- app/src/main/java/com/dicoding/panbas/utils/Injection.kt
+++ app/src/main/java/com/dicoding/panbas/utils/Injection.kt
@@ -1,7 +1,11 @@
 package com.dicoding.panbas.utils
 
 import android.content.Context
+import com.dicoding.panbas.data.datasource.local.ReportLocalDataSource
+import com.dicoding.panbas.data.datasource.local.database.PanbasDatabase
+import com.dicoding.panbas.data.datasource.remote.API.PanbasService
 import com.dicoding.panbas.data.datasource.remote.RemoteDataSource
+import com.dicoding.panbas.data.datasource.remote.request.ReportRemoteDataSource
 import com.dicoding.panbas.data.repository.banjir.BanjirRepository
 import com.dicoding.panbas.data.repository.report.ReportRepository
 
@@ -13,10 +17,14 @@ object Injection {
         return BanjirRepository.getInstance(remoteDataSource)
     }
     fun provideReportRepository(context: Context): ReportRepository {
+        val database = PanbasDatabase.getInstance(context)
+        val localDataSource = ReportLocalDataSource.getInstance(database.reportDao())
 
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val panbasService= PanbasService()
+        val appExecutor = AppExecutor()
+        val remote = ReportRemoteDataSource.getInstance(panbasService.getReportService())
 
-        return ReportRepository.getInstance(remoteDataSource)
+        return ReportRepository.getInstance(remote,localDataSource,appExecutor)
     }
 
 }
